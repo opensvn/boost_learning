5828a5440acb19cc5e3abca85389c034ced01b73
opensvn <justleefeng@gmail.com>
2016-04-11 17:24:55 +0800

    boost asio example written by myself, but can not work

---
+++ hello_asio/client.cpp
@@ -0,0 +1,123 @@
+//
+// Created by root on 16-4-11.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
+#include <boost/function.hpp>
+#include <boost/thread.hpp>
+
+using namespace std;
+using namespace boost::asio;
+using boost::bind;
+
+class Client
+{
+public:
+    Client(io_service &ios, std::string host, int port):
+            sock(ios), ep(ip::address::from_string(host), port),
+            sig(ios)
+    {
+        send_str = "abcdefghijklmnopqrstuvwxyz"
+                   "ABCDEFGHIJKLMNOQPRSTUVWXYZ"
+                   "0123456789!@#$%^&*()-_+=\\|`~;:'\"<>,./?";
+        sig.add(SIGINT);
+        sig.add(SIGTERM);
+        sig.async_wait(bind(&Client::sig_handle, this));
+        is_connected = false;
+        sock.async_connect(ep, bind(&Client::handle_connect, this,
+                                    placeholders::error));
+    }
+private:
+    void handle_connect(const boost::system::error_code &ec)
+    {
+        cout << __func__ << endl;
+        if (!ec)
+        {
+            is_connected = true;
+            shuffle_string();
+            cout << send_str << endl;
+            async_write(sock, buffer(send_str),
+                        bind(&Client::handle_write, this, placeholders::error));
+        }
+        else
+        {
+            sock.close();
+        }
+    }
+
+    void handle_write(const boost::system::error_code &ec)
+    {
+        cout << __func__ << endl;
+        if (!is_connected) return;
+
+        if (!ec)
+        {
+            async_read(sock, buffer(read_buf, send_str.size()),
+                       bind(&Client::handle_read, this, placeholders::error));
+        }
+        else
+        {
+            sock.close();
+        }
+    }
+
+    void handle_read(const boost::system::error_code &ec)
+    {
+        cout << __func__ << endl;
+
+        if (!is_connected) return;
+
+        if (!ec)
+        {
+            cout << read_buf << endl;
+            boost::this_thread::sleep(boost::posix_time::millisec(1000));
+            shuffle_string();
+            async_write(sock, buffer(send_str),
+                        bind(&Client::handle_write, this, placeholders::error));
+        }
+        else
+        {
+            sock.close();
+        }
+    }
+
+    void sig_handle()
+    {
+        cout << __func__ << endl;
+        is_connected = false;
+        sock.close();
+    }
+
+    void shuffle_string()
+    {
+        random_shuffle(send_str.begin(), send_str.end());
+    }
+
+    std::string send_str;
+
+    ip::tcp::socket sock;
+    ip::tcp::endpoint ep;
+    signal_set sig;
+    bool is_connected;
+    enum {max_len = 1024};
+    char read_buf[max_len];
+};
+
+int main(int argc, const char *argv[])
+{
+    if (argc != 3)
+    {
+        cerr << "Usage: client host port" << endl;
+        return -1;
+    }
+
+    io_service ios;
+
+    Client c(ios, argv[1], atoi(argv[2]));
+
+    ios.run();
+    return 0;
+}
---
+++ hello_asio/server.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
+
+using namespace boost::asio;
+using namespace std;
+
+class Server: public boost::enable_shared_from_this<Server>
+{
+public:
+    typedef boost::shared_ptr<ip::tcp::socket> sock_ptr;
+
+    Server(io_service &ios, const ip::tcp::endpoint &ep):
+            ios_(ios), acceptor_(ios, ep)
+    {
+        start_accept();
+    }
+
+private:
+    void start_accept()
+    {
+        new_connection.reset(new ip::tcp::socket(ios_));
+        acceptor_.async_accept(*new_connection,
+                               bind(&Server::handle_accept, this,
+                                   placeholders::error));
+    }
+
+    void handle_accept(const boost::system::error_code &ec)
+    {
+        if (!ec)
+        {
+            async_read(*new_connection, buffer(read_buf),
+                bind(&Server::handle_read, shared_from_this(),
+                    placeholders::error));
+        }
+        start_accept();
+    }
+
+    void handle_read(const boost::system::error_code &ec)
+    {
+        if (!ec)
+        {
+            async_write(*new_connection, buffer(read_buf),
+                bind(&Server::handle_write, shared_from_this(),
+                    placeholders::error));
+            cout << read_buf << endl;
+        }
+    }
+
+    void handle_write(const boost::system::error_code &ec)
+    {
+        ;
+    }
+
+    sock_ptr new_connection;
+    io_service &ios_;
+    ip::tcp::acceptor acceptor_;
+    enum {max_len=1024};
+    char read_buf[max_len];
+    char write_buf[max_len];
+};
+
+int main(int argc, const char *argv[])
+{
+    if (argc != 2)
+    {
+        cerr << "Usage: server port" << endl;
+        return -1;
+    }
+
+    try
+    {
+        io_service ios;
+        ip::tcp::endpoint ep(ip::tcp::v4(), atoi(argv[1]));
+        Server s(ios, ep);
+
+        ios.run();
+    }
+    catch (exception &e)
+    {
+        cerr << e.what() << endl;
+    }
+
+    return 0;
+}
